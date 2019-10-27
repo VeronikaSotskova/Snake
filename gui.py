6529b1dc6e7ff5118a19967e606dbceda79892a8
VeronikaSotskova <verasos@mail.ru>
2019-10-28 00:38:45 +0300

    add score to screen

--- gui.py
+++ gui.py
@@ -1,11 +1,28 @@
 import pygame
+from const import Const
+
+
+def print_text(msg, x, y, font_color, font_type, font_size, window):
+    font = pygame.font.SysFont(font_type, font_size)
+    text = font.render(msg, True, font_color)
+    window.blit(text, (x, y))
+
+
+def draw_lose(window):
+    """Создает экран проигрыша"""
+
+    print_text('Game Over', 30, 100, pygame.Color('Red'), 'monaco', 100, window)
+    print_text('Press Enter to restart, Esc to exit', 100, 240, pygame.Color('Red'), 'monaco', 25, window)
+
 
 class Gui:
     def __init__(self):
 
         self.level = [
+            2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+            2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
             0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-            0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,
+            0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0,
             0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
             0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,
             0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,
@@ -50,20 +67,20 @@ class Gui:
         self.lose = pygame.image.load('images/lose.png')
         self.barrier = []
         self.field = []
-        self.indicator = [[12,12]]
+        self.indicator = [[12,34]]
         self.game = "GAME"
 
     def create_img(self):
         """Создает изображение на основании списка"""
-        screen = pygame.Surface((441, 441), pygame.SRCALPHA, 32)
+        screen = pygame.Surface((Const.SCREEN_WIDTH, Const.SCREEN_HEIGHT), pygame.SRCALPHA, 32)
         x = 1
         y = 1
         for i in self.level:
             if i == 0:
-                pygame.draw.rect(screen, pygame.Color("Grey"), pygame.Rect(x,y,10,10))
-            x += 11
-            if x == 441:
-                y += 11
+                pygame.draw.rect(screen, pygame.Color("Grey"), pygame.Rect(x,y,Const.SIZE_BLOCK,Const.SIZE_BLOCK))
+            x += Const.SIZE_BLOCK + Const.SIZE_SPACE
+            if x == Const.SCREEN_WIDTH:
+                y += Const.SIZE_BLOCK + Const.SIZE_SPACE
                 x = 1
         pygame.image.save(screen, "images/gui2.png")
 
@@ -78,29 +95,25 @@ class Gui:
         for i in self.level:
             if i == 0:
                 self.barrier.append([x, y])
-            elif i == 1 and y != 12:
+            elif i == 1:
                 self.field.append([x, y])
-            x += 11
+            x += Const.SIZE_BLOCK + Const.SIZE_SPACE
             if x == 441:
-                y += 11
+                y += Const.SIZE_BLOCK + Const.SIZE_SPACE
                 x = 1
 
     def get_new_indicator(self):
         """Заполняет список координатами индикатора"""
-        self.indicator.append([self.indicator[-1][0] + 11, 12])
+        self.indicator.append([self.indicator[-1][0] + Const.SIZE_BLOCK + Const.SIZE_SPACE, 34])
 
     def draw_indicator(self, window):
         """Отрисовка индикатора"""
         for i in self.indicator:
-            pygame.draw.rect(window, pygame.Color("Green"), pygame.Rect(i[0], i[1], 10, 10))
+            pygame.draw.rect(window, pygame.Color("Green"), pygame.Rect(i[0], i[1], Const.SIZE_BLOCK, Const.SIZE_BLOCK))
 
     def draw_win(self, window):
         """Создает победный экран"""
         window.blit(self.win, (23, 100))
-
-    def draw_lose(self, window):
-        """Создает экран проигрыша"""
-        window.blit(self.lose, (23, 100))
 
     def check_win_or_lose(self):
         """Отслеживает выиграл или проиграл игрок"""
@@ -108,3 +121,7 @@ class Gui:
             self.game = "LOSE"
         elif len(self.indicator) == 37:
             self.game = "WIN"
+
+    def show_score(self, window):
+        """Отображение результата"""
+        print_text('Score: {0}'.format(len(self.indicator)-1), 1, 5, pygame.Color("Red"), 'monaco', 24, window)
