97e3c8b1b045d0147c3d4d875f51132ae949dff0
VeronikaSotskova <verasos@mail.ru>
2019-11-03 00:26:28 +0300

    fix bugs

--- control.py
+++ control.py
@@ -1,28 +1,5 @@
-import pygame
-from pygame.locals import *
-
-
 class Control:
     def __init__(self):
         self.run = True
         self.direction = "RIGHT"
         self.pause = False
-
-    def control(self):
-        """Управление в зависимости от флага"""
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                self.run = False
-            elif event.type == KEYDOWN:
-                if event.key == K_RIGHT and self.direction != "LEFT":
-                    self.direction = "RIGHT"
-                elif event.key == K_LEFT and self.direction != "RIGHT":
-                    self.direction = "LEFT"
-                elif event.key == K_UP and self.direction != "DOWN":
-                    self.direction = "UP"
-                elif event.key == K_DOWN and self.direction != "UP":
-                    self.direction = "DOWN"
-                elif event.key == K_ESCAPE:
-                    self.run = False
-                elif event.key == K_SPACE:
-                    self.pause = not self.pause
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from gui import Gui, print_text, draw_lose
 from food import Food
 from const import Const
 from button import Button
+from pygame.locals import *
 
 pygame.init()
 window = pygame.display.set_mode((Const.SCREEN_WIDTH, Const.SCREEN_HEIGHT))
@@ -24,6 +25,9 @@ def restart_settings():
                   [Const.START_X_HEAD - 22, Const.START_Y_HEAD]]
     gui.indicator = [[12, 34]]
     gui.game = "GAME"
+    control.direction = "RIGHT"
+    control.pause = True
+    control.run = True
 
 
 def show_menu():
@@ -35,12 +39,14 @@ def show_menu():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
         window.blit(menu_bg, (0, 0))
         print_text('SNAKE', 150, 20, pygame.Color('Black'), 'monaco', 60, window)
-        # TODO: если начать игру и выйти из нее, а потом опять нажать на кнопку ничего не происходит
         button.draw(100,100, 'Start', window, (0,0,0), 'monaco', 25, start_game)
         button.draw(100,200, 'Records', window, (0,0,0), 'monaco', 25)
-        button.draw(100,300, 'Exit', window, (0,0,0), 'monaco', 25)
+        button.draw(100,300, 'Exit', window, (0,0,0), 'monaco', 25, exit)
         pygame.display.update()
         pygame.display.flip()
 
@@ -52,8 +58,31 @@ def start_game():
 
 def game_cycle():
     while control.run:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                control.run = False
+                pygame.quit()
+            elif event.type == KEYDOWN:
+                if event.key == K_RIGHT and control.direction != "LEFT":
+                    control.direction = "RIGHT"
+                elif event.key == K_LEFT and control.direction != "RIGHT":
+                    control.direction = "LEFT"
+                elif event.key == K_UP and control.direction != "DOWN":
+                    control.direction = "UP"
+                elif event.key == K_DOWN and control.direction != "UP":
+                    control.direction = "DOWN"
+                elif event.key == K_ESCAPE:
+                    control.run = False
+                elif event.key == K_SPACE:
+                    control.pause = not control.pause
+                elif event.key == pygame.K_RETURN and (gui.game == 'LOSE' or gui.game == 'WIN'):
+                    control.run = True
+                    control.pause = True
+                    gui.game = "GAME"
+                    restart_settings()
+                    game_cycle()
+
         gui.check_win_or_lose()
-        control.control()
         window.fill(pygame.Color('Black'))
 
         if gui.game == "GAME":
@@ -67,18 +96,6 @@ def game_cycle():
         # если поражение
         elif gui.game == "LOSE":
             draw_lose(window)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    quit()
-    # TODO: при нажатии на enter игра не всегда перезапускается
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RETURN:
-                        control.run = True
-                        control.pause = False
-                        gui.game = "GAME"
-                        restart_settings()
-                        game_cycle()
 
         gui.show_score(window)
         gui.draw_indicator(window)
